633e483e36f1b12273d670abc8f32c068534bb52
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-06-27 12:03:33 -0300

    Added Habit and Objective

---
+++ GoodHabitsCanvas/Model/Habit.swift
@@ -0,0 +1,31 @@
+//
+//  Habit.swift
+//  GoodHabitsCanvas
+//
+//  Created by Luciano Sclovsky on 27/06/19.
+//  Copyright © 2019 Luciano Sclovsky. All rights reserved.
+//
+
+import Foundation
+
+enum Frequency {
+    case daily
+    case weekly
+    case monthly
+    case quartely // maybe keep maybe delete
+    case yearly
+}
+
+enum Repetition {
+    case single
+    case multiple
+}
+
+struct Habit {
+    var description: String
+    var objective: Objective
+    var frequency: Frequency
+    var repetition: Repetition
+    var trackTime: Bool
+    var tracking: [Date]
+}
---
+++ GoodHabitsCanvas/Model/Objective.swift
@@ -0,0 +1,15 @@
+//
+//  Objective.swift
+//  GoodHabitsCanvas
+//
+//  Created by Luciano Sclovsky on 27/06/19.
+//  Copyright © 2019 Luciano Sclovsky. All rights reserved.
+//
+
+import Foundation
+
+struct Objective {
+    var description: String
+    var slice: WheelSlice
+    var isValue: Bool
+}
